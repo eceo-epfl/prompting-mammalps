5ebabaa1185e52fa174cac4b45f17f7452a79aa4
gabeva <valentin.gabeff@epfl.ch>
2026-07-21 16:47:44 +0200

    fix: fixed file_path

--- models/llm/scripts/run_llm_agent.sh
+++ models/llm/scripts/run_llm_agent.sh
@@ -2,7 +2,7 @@
 DATA_ROOT="$1"
 
 python3 run.py \
-    -IQ "$DATA_ROOT/metadata/queries_and_videos.json" \
+    -IQ "$DATA_ROOT/metadata/queries_and_videos_train.json" \
     -D "$DATA_ROOT" \
     -O ./results/ \
     --llm "qwen"
